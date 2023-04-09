7decd462ee4f38b6dbb0b8fb633fa2fdadf7fd31
fierylion <mawalladaniel2018@gmail.com>
2023-04-09 11:03:18 +0300

    added a fallback documentation url

--- DawaFasta/urls.py
+++ DawaFasta/urls.py
@@ -17,8 +17,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
