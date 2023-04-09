23545efe5ada29cfa4431d2480ab77e3750e0834
fierylion <mawalladaniel2018@gmail.com>
2023-04-09 09:41:54 +0300

    added mimetypes

--- DawaFasta/settings.py
+++ DawaFasta/settings.py
@@ -13,6 +13,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import mimetypes
+
+mimetypes.add_type('text/css', '.css', True)
+mimetypes.add_type('text/js', '.js', True)
 load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
